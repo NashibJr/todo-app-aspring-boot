899ad2f4884e5aabb03657d0440bf264a3c2d062
NashibJr <nashibjr@gmail.com>
2025-07-27 22:17:21 +0300

    Feat: Added the user entity and the repository

---
+++ src/main/java/com/example/todo/todo/entities/UserEntity.java
@@ -0,0 +1,31 @@
+package com.example.todo.todo.entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity {
+    @Id
+    @JsonProperty("_id")
+    private String id;
+
+    @NotNull(message = "This field is required")
+    private String username;
+    
+    @Email(message = "Enter a valid email address")
+    @NotNull(message = "This field is required")
+    private String email;
+
+    private String password;
+}
---
+++ src/main/java/com/example/todo/todo/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.todo.todo.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.todo.todo.entities.UserEntity;
+
+@Repository
+public interface UserRepository extends MongoRepository<UserEntity, String> {
+
+}
